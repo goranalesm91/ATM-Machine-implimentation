0c59b7c7c61068877215ae14946bde43d50f365f
Siddharam goranale <99482898+goranalesm91@users.noreply.github.com>
2022-08-17 11:38:18 +0530

    ATM imlimentation

---
+++ AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package AtmImplimentation;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
